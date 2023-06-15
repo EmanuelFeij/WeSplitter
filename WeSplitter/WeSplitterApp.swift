ed5da176c57da9e06602ab74b8b2b94952d8cd04
EmanuelFeij <50448386+EmanuelFeij@users.noreply.github.com>
2023-06-15 17:44:58 +0100

    project start

---
+++ WeSplitter/WeSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitterApp.swift
+//  WeSplitter
+//
+//  Created by Emanuel Feijó on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
