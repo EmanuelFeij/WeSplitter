aae82ed8de31bc576b3e0ab6c98e97343ca39561
EmanuelFeij <50448386+EmanuelFeij@users.noreply.github.com>
2023-06-15 22:49:39 +0100

    project start

--- WeSplitter/ContentView.swift
+++ WeSplitter/ContentView.swift
@@ -7,15 +7,67 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipsPercentages : [Int] = [0, 10, 15,20, 25]
+    
+    var totalPerson : Double {
+        let peopleCount = Double(numOfPeople + 2)
+        let tipPercentage = Double(tipPercentage)
+        
+        let tipValue = checkAmount * (tipPercentage / 100)
+        let allTotal = checkAmount + tipValue
+        
+        return allTotal / peopleCount
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+                Form {
+                    Section{
+                        TextField("Amount",value: $checkAmount,
+                            format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                        )
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                        
+                        Picker("Number of People", selection: $numOfPeople) {
+                            ForEach(2..<100){
+                                Text("\($0) People")
+                            }
+                        }
+                    }
+                    Section () {
+                        Picker("Tip Percentage", selection: $tipPercentage){
+                            ForEach(tipsPercentages, id: \.self){
+                                Text("\($0)%")
+                            }
+                        }.pickerStyle(.segmented)
+                    } header: {
+                        Text("How much do you want to leave ?")
+                    }
+                    
+                    Section {
+                        Text(totalPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused.toggle()
+                    }
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
--- WeSplitter/WeSplitterApp.swift
+++ WeSplitter/WeSplitterApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct WeSplitterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack{
+                ContentView()
+            }
         }
     }
 }
